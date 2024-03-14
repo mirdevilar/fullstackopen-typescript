78b297a4f57bf2c33efeed95643de264e253275e
alekarhis <alekarhis@pm.me>
2024-03-14 13:20:22 +0200

    use arguments in bmi

--- b/bmiCalculator.ts
+++ b/bmiCalculator.ts
@@ -1,3 +1,10 @@
+import _ from 'lodash'
+
+interface Values {
+  height: number;
+  weight: number;
+}
+
 const calculateBmi = (height: number, weight: number): string => {
   if (height <= 0) {
     throw new Error('Invalid height!');
@@ -14,4 +21,30 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 }
 
-console.log(calculateBmi(0, 74))
+const parseArguments = (args: string[]): Values => {
+  if (args.length < 4) throw new Error('Not enough arguments!')
+  if (args.length > 4) throw new Error('Too many arguments!')
+
+  const height = Number(args[2])
+  const weight = Number(args[3])
+
+  if (_.isNaN(height) || _.isNaN(weight)) {
+    throw new Error('Values must be numbers!')
+  }
+  
+  return {
+    height: Number(args[2]),
+    weight: Number(args[3]),
+  }
+}
+
+try {
+  const { height, weight } = parseArguments(process.argv)
+  console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong... '
+  if (error instanceof Error) {
+    errorMessage += 'Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
